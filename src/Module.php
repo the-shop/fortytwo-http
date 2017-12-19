8cef1dc94167a21f37f93fa8b99152d92d440198
ivankristic <thechosen.os@gmail.com>
2017-12-19 14:46:33 +0100

    update - accommodated config refactor - added `JSON_PRETTY_PRINT` argument to `Json` renderer before `echo` to make   output more readable for now - changed fortytwo-base dependency to `dev-master` while still in heavy dev mode

--- src/Module.php
+++ src/Module.php
@@ -13,10 +13,29 @@ class Module extends BaseModule
     /**
      * @inheritdoc
      */
-    public function bootstrap()
+    public function loadConfig()
     {
         // Let's read all files from module config folder and set to Configuration
         $configDirPath = realpath(dirname(__DIR__)) . '/config/';
         $this->setModuleConfiguration($configDirPath);
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap()
+    {
+        $application = $this->getApplication();
+
+        $appConfig = $application->getConfiguration();
+
+        // Add routes to dispatcher
+        if (
+            empty($routes = $appConfig->getPathValue('routes')) === false
+        ) {
+            $application->getDispatcher()
+                        ->addRoutes($routes);
+        }
     }
 }
